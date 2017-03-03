3fa8d4c72192b0e3dca7fa662dc81ecaf0330e81
Chris Janes <gingoreilly@gmail.com>
2017-03-03 15:20:08 +0000

    build test framework

---
+++ portfolio/LinkedList/LinkedList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework.Constraints;
+
+namespace portfolio.LinkedList
+{
+
+    class LinkedList<T>
+    {
+        public int Length { get; private set; }
+
+        public class Node<T>
+        {
+            public T Data;
+            public Node<T> NextNode;
+        }
+
+        public void InsertBeginning(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertAfter(int index, T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T At(int index)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ portfolio/LinkedList/LinkedListTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace portfolio.LinkedList
+{
+    [TestFixture]
+    class LinkedListTest
+    {
+        [Test]
+        public void InsertBeginning()
+        {
+            var list = new LinkedList<int>();
+
+            list.InsertBeginning(1);
+            Assert.That(list.Length, Is.EqualTo(1));
+            Assert.That(list.At(0), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void InsertAfter()
+        {
+            var list = new LinkedList<int>();
+
+            list.InsertBeginning(1);
+
+            list.InsertAfter(0, 5);
+            Assert.That(list.Length, Is.EqualTo(2));
+            Assert.That(list.At(0), Is.EqualTo(1));
+            Assert.That(list.At(1), Is.EqualTo(5));
+        }
+
+        [Test]
+        public void RemoveBeginning()
+        {
+            
+        }
+
+        [Test]
+        public void RemoveAfter()
+        {
+            
+        }
+
+        [Test]
+        public void Length()
+        {
+            
+        }
+    }
+}
